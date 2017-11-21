9eaf06adc62c404529aff4b5372e9dd0fd0904f5
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-11-20 16:13:09 -0800

    Fixed server bug, added textarea component

--- src/client/components/starter/index.js
+++ src/client/components/starter/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Footer from '../footer';
 
 export default class Starter extends Component {
   render() {
     return (
     <div className="hello-world">
       <h1>Hello World!</h1>
+      <Footer />
     </div>
     );
   }
