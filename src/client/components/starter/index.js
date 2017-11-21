1fd1d67550e2076e0177a3d185b56efbae2d787c
ontoneio <ontoneio.mm@gmail.com>
2017-11-20 17:49:02 -0800

    Create View component and attempt to render canvas to DOM

--- src/client/components/starter/index.js
+++ src/client/components/starter/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import View from '../view'
 
 export default class Starter extends Component {
   render() {
     return (
     <div className="hello-world">
       <h1>Hello World!</h1>
+      <View/>
     </div>
     );
   }
