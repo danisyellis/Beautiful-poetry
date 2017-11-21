4b0b7302edc2cd51380af1336aa1ab65318373cd
Danielle Gellis <danisyellis@gmail.com>
2017-11-20 18:03:17 -0800

    add a name component for the app name

---
+++ src/client/components/Name/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Name extends Component {
+  render() {
+    return (
+      <div className="App-name">
+      <h1> Poetion </h1>
+      </div>
+    )
+  }
+}
+
+
+
+export default Name
--- src/client/components/starter/index.js
+++ src/client/components/starter/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Footer from '../footer';
+import Name from '../Name';
 
 export default class Starter extends Component {
   render() {
     return (
     <div className="hello-world">
+      <Name />
       <h1>Hello World!</h1>
       <Footer />
     </div>
