b0d02c9b11d66842ad4d1f9b234b6aee89a84efc
smithrm941 <smithrm941@gmail.com>
2017-11-21 09:17:20 -0800

    credits component renders

--- src/client/components/starter/index.js
+++ src/client/components/starter/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Footer from '../footer';
+import Credits from '../credits';
 
 export default class Starter extends Component {
   render() {
     return (
-    <div className="hello-world">
-      <h1>Hello World!</h1>
-      <Footer />
-    </div>
+      <div className="hello-world">
+        <Credits />
+        <h1>Hello World!</h1>
+        <Footer />
+      </div>
     );
   }
 }
