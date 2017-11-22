fbb0f04e3055d5ba3ef31f66bcffc2cef494abf2
smithrm941 <smithrm941@gmail.com>
2017-11-21 17:45:18 -0800

    made bottom bar larger, added z-indexes so it covers design credits when pushed up but not app name

--- src/client/components/credits/index.js
+++ src/client/components/credits/index.js
@@ -4,13 +4,13 @@ class Credits extends Component {
   render() {
     return (
       <div className="credits">
+        <h6>Designed and coded by:</h6>
         <ul>
-        <li>Designed and coded by:</li>
-        <li><a href="http://www.github.com/danisyellis" target="_blank">Dani Gellis</a></li>
-        <li><a href="http://www.github.com/ontoneio" target="_blank">Jonathan Mitchell</a></li>
-        <li><a href="http://www.github.com/pkallas" target="_blank">Patrick Kallas</a></li>
-        <li><a href="http://www.github.com/smithrm941" target="_blank">Rhonda Smith</a></li>
-        <li><a href="http://www.github.com/sashajustice" target="_blank">Sabrin Nasreddin</a></li>
+          <li><a href="http://www.github.com/danisyellis" target="_blank">Dani Gellis</a></li>
+          <li><a href="http://www.github.com/ontoneio" target="_blank">Jonathan Mitchell</a></li>
+          <li><a href="http://www.github.com/pkallas" target="_blank">Patrick Kallas</a></li>
+          <li><a href="http://www.github.com/smithrm941" target="_blank">Rhonda Smith</a></li>
+          <li><a href="http://www.github.com/sashajustice" target="_blank">Sabrin Nasreddin</a></li>
         </ul>
       </div>
     );
